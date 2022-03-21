16a316a73fd803e6cfed14d619dfcdbe0a1e49fd
user <user@gmail.net>
2022-03-21 18:15:34 +0500

    4

--- main/views.py
+++ main/views.py
@@ -3,6 +3,11 @@ from .models import *
 
 
 def home(request):
-    items = Item.objects.all()
+    if request.GET.get('category'):
+        category = Category.objects.get(pk=request.GET['category'])
+        items = Item.objects.filter(category=category)
+    else:
+        items = Item.objects.all()
+
     categories = Category.objects.all()
     return render(request, 'home.html', {'items': items, 'categories': categories})
